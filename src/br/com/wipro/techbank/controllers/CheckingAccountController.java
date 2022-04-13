ada76f4427d4501e020e306256294ee00c796c4e
renatomak <renato.mark.silva@gmail.com>
2022-04-12 22:33:10 -0300

    feat: checking account controller

---
+++ src/br/com/wipro/techbank/controllers/CheckingAccountController.java
@@ -0,0 +1,31 @@
+package br.com.wipro.techbank.controllers;
+
+import java.util.Scanner;
+
+public class CheckingAccountController extends ManagerAbstract{
+
+    @Override
+    public void update(Scanner scan) {
+        System.out.println("Falta implementar o método UPDATE em Conta Corrente");
+    }
+
+    @Override
+    public void findById(Scanner scan) {
+        System.out.println("Falta implementar o método FINDBYID  em Conta Corrente");
+    }
+
+    @Override
+    public void findAll() {
+        System.out.println("Falta implementar o método FINDALL  em Conta Corrente");
+    }
+
+    @Override
+    public void delete(Scanner scan) {
+        System.out.println("Falta implementar o método DELETE  em Conta Corrente");
+    }
+
+    @Override
+    public void save(Scanner scan) {
+        System.out.println("Falta implementar o método SAVE  em Conta Corrente");
+    }
+}
