086e6daf117b3ec3a5e59db51d4e4121750e2e7d
renatomak <renato.mark.silva@gmail.com>
2022-04-14 20:14:08 -0300

    refact: class credit card

--- src/br/com/wipro/techbank/models/CreditCard.java
+++ src/br/com/wipro/techbank/models/CreditCard.java
@@ -1,4 +1,26 @@
 package br.com.wipro.techbank.models;
 
+import java.util.Date;
+
 public class CreditCard {
+
+    private String cardNumber;
+    private Date expirationDate;
+    private Short securityCode;
+    private Double limit;
+    private Double usedLimit;
+
+    public CreditCard(String cardNumber, Date expirationDate, Short securityCode, Double limit, Double usedLimit) {
+        this.cardNumber = cardNumber;
+        this.expirationDate = expirationDate;
+        this.securityCode = securityCode;
+        this.limit = limit;
+        this.usedLimit = usedLimit;
+    }
+
+    public void toBuy(Double value) {
+        this.usedLimit += value;
+    }
+
+
 }
